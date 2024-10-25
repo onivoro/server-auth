990a9d30b2b3958e5df9956c45ed745f14341a67
icedlee337 <lee.e.norris@gmail.com>
2024-10-24 21:33:06 -0500

    chore: bump deps; node 22

--- src/lib/classes/abstract-auth-guard.class.ts
+++ src/lib/classes/abstract-auth-guard.class.ts
@@ -1,11 +1,10 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { authorizeRequest } from '../functions/authorize-request.function';
 
 export abstract class AbstractAuthGuard<TAccessToken> implements CanActivate {
   canActivate(
     context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ) {
     return authorizeRequest<TAccessToken>(context, (token, request) => this.evaluateToken(token, request));
   };
 
